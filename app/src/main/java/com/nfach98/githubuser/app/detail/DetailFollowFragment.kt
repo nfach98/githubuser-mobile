df8e99a06fe7f3b7f2683094d614a9f68ea3e96d
nfach98 <nfach98@gmail.com>
2021-03-28 20:16:41 +0700

    detail list follow

--- app/src/main/java/com/nfach98/githubuser/app/detail/DetailFollowFragment.kt
+++ app/src/main/java/com/nfach98/githubuser/app/detail/DetailFollowFragment.kt
@@ -2,14 +2,12 @@ package com.nfach98.githubuser.app.detail
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.nfach98.githubuser.R
 import com.nfach98.githubuser.app.main.MainActivity
 import com.nfach98.githubuser.app.main.MainUserAdapter
 import com.nfach98.githubuser.databinding.FragmentDetailFollowBinding
@@ -35,12 +33,13 @@ class DetailFollowFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_detail_follow, container, false)
+        binding = FragmentDetailFollowBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = FragmentDetailFollowBinding.inflate(layoutInflater)
+
         val index = arguments?.getInt(DETAIL_PAGE, 0)
         val username = arguments?.getString(DETAIL_USERNAME)
 
@@ -49,10 +48,26 @@ class DetailFollowFragment : Fragment() {
         binding.rvFollow.setHasFixedSize(true)
         binding.rvFollow.layoutManager = LinearLayoutManager(context)
 
-        when(index){
-            0 -> {
-                if (username != null) {
+        if (username != null) {
+            when(index){
+                0 -> {
                     viewModel.getUserFollower(username).observe(this, {
+                        if(it != null){
+                            val adapter = MainUserAdapter(it)
+                            adapter.setOnItemClickCallback(object : MainUserAdapter.OnItemActionCallback {
+                                override fun onItemClicked(data: Item) {
+                                    val intent = Intent(context, DetailActivity::class.java)
+                                    intent.putExtra(MainActivity.EXTRA_USER, data)
+                                    startActivity(intent)
+                                }
+                            })
+
+                            binding.rvFollow.adapter = adapter
+                        }
+                    })
+                }
+                1 -> {
+                    viewModel.getUserFollowing(username).observe(this, {
                         if(it != null){
                             val adapter = MainUserAdapter(it)
                             adapter.setOnItemClickCallback(object : MainUserAdapter.OnItemActionCallback {
