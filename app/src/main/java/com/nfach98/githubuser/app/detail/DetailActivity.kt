e4f0d177c19942fdb0b30b31bc3919c092a3f5cd
nfach98 <nfach98@gmail.com>
2021-04-12 11:00:38 +0700

    test rollback

--- app/src/main/java/com/nfach98/githubuser/app/detail/DetailActivity.kt
+++ app/src/main/java/com/nfach98/githubuser/app/detail/DetailActivity.kt
@@ -24,6 +24,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class DetailActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityDetailBinding
     private lateinit var username: String
     private lateinit var userDetail: UserDetail
